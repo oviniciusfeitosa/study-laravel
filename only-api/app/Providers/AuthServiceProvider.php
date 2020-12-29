7cc623fd04be7497343ae49c3ba65ab1df136cad
Vinicius Feitosa da Silva <viniciusfesil@gmail.com>
2020-12-29 00:48:46 -0300

    ✨ API authentication / Project CRUD / Routes

--- only-api/app/Providers/AuthServiceProvider.php
+++ only-api/app/Providers/AuthServiceProvider.php
@@ -3,28 +3,18 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The policy mappings for the application.
-     *
-     * @var array
-     */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        'App\Models\Model' => 'App\Policies\ModelPolicy',
     ];
 
-    /**
-     * Register any authentication / authorization services.
-     *
-     * @return void
-     */
-    public function boot()
+    public function boot(): void
     {
         $this->registerPolicies();
 
-        //
+        Passport::routes();
     }
 }
